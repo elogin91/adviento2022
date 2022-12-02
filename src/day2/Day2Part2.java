f9d4d83d4f3d0db521ce535131f0eff54de226b6
Elogin <86604232+elogin91@users.noreply.github.com>
2022-12-02 11:56:12 +0100

    day2

---
+++ src/day2/Day2Part2.java
@@ -0,0 +1,82 @@
+package day2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Day2Part2 {
+    public static void main(String[] args) {
+
+        File file = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+
+            file = new File ("inputData/dia2.txt");
+            fr = new FileReader (file);
+            br = new BufferedReader(fr);
+
+            String line;
+            int accumulatedOfPoints=0;
+            String player1Option;
+            String player2Option;
+
+            while((line=br.readLine())!=null) {
+                System.out.println(line);
+                System.out.println(player1Option= line.substring(0,1));
+                System.out.println(player2Option= line.substring(line.length()-1));
+                //LOSE
+                if(player2Option.equals("X")){
+                    if(player1Option.equals("A")){
+                        accumulatedOfPoints=accumulatedOfPoints+3;
+                    }
+                    else if(player1Option.equals("B")){
+                        accumulatedOfPoints=accumulatedOfPoints+1;
+                    }
+                    else{
+                        accumulatedOfPoints=accumulatedOfPoints+2;
+                    }
+                }
+                //DRAW
+                if(player2Option.equals("Y")){
+                    if(player1Option.equals("A")){
+                        accumulatedOfPoints=accumulatedOfPoints+4;
+                    }
+                    else if(player1Option.equals("B")){
+                        accumulatedOfPoints=accumulatedOfPoints+5;
+                    }
+                    else{
+                        accumulatedOfPoints=accumulatedOfPoints+6;
+                    }
+                }
+                //WIN
+                if(player2Option.equals("Z")){
+                    if(player1Option.equals("A")){
+                        accumulatedOfPoints=accumulatedOfPoints+8;
+                    }
+                    else if(player1Option.equals("B")){
+                        accumulatedOfPoints=accumulatedOfPoints+9;
+                    }
+                    else{
+                        accumulatedOfPoints=accumulatedOfPoints+7;
+                    }
+                }
+
+            }
+            System.out.println(accumulatedOfPoints);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+    }
+}
